76867de1c3c0aacadfaaa3171db9b080197169b8
Nick Lanzo <nlanzo@gmail.com>
2025-05-14 17:01:15 -0500

    add collision detection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -30,6 +31,12 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
 
         screen.fill("black")
 
