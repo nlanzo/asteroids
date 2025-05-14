779d90186a21e5a793adbeb964479f16c938728b
Nick Lanzo <nlanzo@gmail.com>
2025-05-14 16:27:57 -0500

    refactor main.py for readability

--- main.py
+++ main.py
@@ -3,23 +3,35 @@ from constants import *
 from player import Player
 
 def main():
-  pygame.init()
-  print("Starting Asteroids!")
-  print(f"Screen width: {SCREEN_WIDTH}")
-  print(f"Screen height: {SCREEN_HEIGHT}")
-  clock = pygame.time.Clock()
-  dt = 0
-  display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
-  while True:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        return
-    pygame.Surface.fill(display, color=(0,0,0))
-    player.update(dt)
-    player.draw(screen=display)
-    pygame.display.flip()
-    dt = clock.tick(60) / 1000
+    pygame.init()
+    display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        updatable.update(dt)
+
+        display.fill("black")
+
+        for item in drawable:
+            item.draw(screen=display)
+
+        pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
   main()
