cfcf7747fb9002052ea5144888ee29c8a1ac3604
Nick Lanzo <nlanzo@gmail.com>
2025-05-14 21:11:10 -0500

    add asteroids split and accelerate after being destroyed

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import CircleShape
 from constants import *
+import random
 
 
 class Asteroid(CircleShape):
@@ -13,4 +14,15 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
 
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20, 51)
+        new_velocity_1 = self.velocity.rotate(random_angle)
+        new_velocity_2 = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid_1, asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius), Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_1.velocity, asteroid_2.velocity = new_velocity_1 * 1.2, new_velocity_2 * 1.2
 
+        
